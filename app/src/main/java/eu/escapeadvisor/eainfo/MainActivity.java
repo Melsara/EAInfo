6073610253332332df7aea6fbf78379caa9bdf91
Melsara <kaxhortz@gmail.com>
2018-02-17 15:21:49 +0100

    More elegant way to manage strings.

--- app/src/main/java/eu/escapeadvisor/eainfo/MainActivity.java
+++ app/src/main/java/eu/escapeadvisor/eainfo/MainActivity.java
@@ -13,16 +13,18 @@ public class MainActivity extends AppCompatActivity {
     String business;
     String address1;
     String about;
+    String object;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mail = new Intent(Intent.ACTION_SENDTO);
-        website = "http://www.escapeadvisor.eu";
-        business = "Escape Advisor";
-        address1 = "Always open";
-        about = "Lorem ipsum";
+        website = getString(R.string.website);
+        business = getString(R.string.cardview3_business);
+        address1 = getString(R.string.cardview3_address1);
+        object = getString(R.string.email_object);
+        about = getString(R.string.cardview3_about);
 
     }
 
@@ -33,8 +35,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendEmail(View view) {
         mail.setData(Uri.parse("mailto:"));
-        mail.putExtra(Intent.EXTRA_TEXT, business + "\n"+ address1 + "\n" + about);
+        mail.putExtra(Intent.EXTRA_TEXT, object);
         mail.putExtra(Intent.EXTRA_SUBJECT, (getString((R.string.email_subject))));
+        mail.putExtra(Intent.EXTRA_EMAIL,new String[] { getString(R.string.email_recipient) });
         if (mail.resolveActivity(getPackageManager()) != null) {
             startActivity(mail);
         }
